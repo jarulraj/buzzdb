45bc8f5846448757a9000ec409e847a12fe153c7
Joy Arulraj <arulraj@gatech.edu>
2023-07-12 23:52:17 -0400

    updates

--- generate-data.cpp
+++ generate-data.cpp
@@ -5,14 +5,15 @@
 int main() {
     std::random_device rd;
     std::mt19937 gen(rd());
-    // For customer IDs between 1 and 10
-    std::uniform_int_distribution<> dis1(1, 10); 
-    // For amounts between 1 and 100    
-    std::uniform_int_distribution<> dis2(1, 100);
+    // For customer IDs between 1 and 9
+    std::uniform_int_distribution<> dis1(1, 9); 
+    std::poisson_distribution<> dis2(100);
+
+    int number_of_sales = 1000;
 
     std::ofstream outputFile("output.txt");
     if (outputFile.is_open()) {
-        for (int i = 0; i < 1000; ++i) {
+        for (int i = 0; i < number_of_sales; ++i) {
             int customerId = dis1(gen);
             int amount = dis2(gen);
             outputFile << customerId << " " << amount << "\n";
