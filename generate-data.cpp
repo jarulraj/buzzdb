a4c3bf0b4aa01fad8cc8cca4dbf778b12dddc3e0
Joy Arulraj <arulraj@gatech.edu>
2023-07-16 13:40:33 -0400

    updates

--- generate-data.cpp
+++ generate-data.cpp
@@ -5,23 +5,27 @@
 int main() {
     std::random_device rd;
     std::mt19937 gen(rd());
-    // For customer IDs between 1 and 9
-    std::uniform_int_distribution<> dis1(1, 9); 
-    std::poisson_distribution<> dis2(100);
 
-    int number_of_sales = 10000000;
+    std::default_random_engine generator(std::random_device{}());
+    std::uniform_int_distribution<int> amountDistribution(1, 1000);
+    std::discrete_distribution<int> customerDistribution({40, 20, 10, 10, 5, 5, 4, 3, 2, 1});
+
+    int number_of_sales = 10 * 1000 * 1000;
 
     std::ofstream outputFile("output.txt");
     if (outputFile.is_open()) {
         for (int i = 0; i < number_of_sales; ++i) {
-            int customerId = dis1(gen);
-            int amount = dis2(gen);
+            // +1 to get IDs from 1 to 10
+            int customerId = customerDistribution(gen) + 1;
+            int amount = amountDistribution(gen);
             outputFile << customerId << " " << amount << "\n";
         }
         outputFile.close();
     } else {
         std::cerr << "Unable to open file";
     }
+
+    outputFile.close();
 
     return 0;
 }
