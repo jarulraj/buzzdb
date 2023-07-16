06ee99582e640bc0486f8fa5b17b50e1b5f590d8
Joy Arulraj <arulraj@gatech.edu>
2023-07-16 13:46:36 -0400

    updates

---
+++ compile.py
@@ -0,0 +1,31 @@
+import subprocess
+import concurrent.futures
+import glob
+
+# List of C++ files to compile
+# Use glob to get all .cpp files
+cpp_files = glob.glob('*.cpp')
+
+def compile_cpp(file):
+    # Define the command to compile the C++ file
+    command = f"g++ -fdiagnostics-color -std=c++11 -O3 -Wall -Werror -Wextra {file} -o {file.split('.')[0]}.out"
+    
+    # Run the command
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    
+    # Wait for the process to complete and get the output
+    stdout, stderr = process.communicate()
+    
+    # Check if the process had an error
+    if process.returncode != 0:
+        print(f"Error while compiling {file}:")
+        print(stderr.decode())
+    else:
+        print(f"Compiled {file} successfully.")
+        print(stdout.decode())
+
+if __name__ ==  '__main__':
+    # Create a ProcessPoolExecutor
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        # Use the executor to compile all C++ files in parallel
+        executor.map(compile_cpp, cpp_files)
